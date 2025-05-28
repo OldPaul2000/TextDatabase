c84e1f2b1c14240bb9dfe562c394a82529863673
Batrin Paul <paulbatrin@yahoo.com>
2025-05-28 20:20:17 +0300

    code-cleaning

--- src/main/java/com/textdatabase/Main.java
+++ src/main/java/com/textdatabase/Main.java
@@ -3,14 +3,6 @@ package com.textdatabase;
 public class Main {
     public static void main(String[] args) {
 
-        TextDatabase database = TextDatabase.builder()
-                .fileLocation("C:\\Users\\paulb\\Desktop")
-                .fileName("ValuesDatabase.txt")
-                .structureType(DatabaseStructureType.VALUE)
-                .objectType(Car.class)
-                .build();
-
-
 
     }
 
